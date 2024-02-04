5df5e3d197b9bb559ed63c863db16697d9ef1ff8
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-02-04 14:35:03 +0500

    added: chakra theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from './lib/theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>,
 )
+
