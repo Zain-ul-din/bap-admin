a0cd411e742161b60282f5b50bf7165dd77bfe9e
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-02-05 14:39:10 +0500

    added: routes

---
+++ src/components/lib/constant.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  'Dashboard': '/dashboard',
+  'Organization': '/organization',
+  'Ambulance': '/ambulance',
+  'Users': '/users',
+  'Booking': '/booking',
+  'Settings': '/settings',
+  'Emergency contact': '/emergency-contact'
+}
+
+
