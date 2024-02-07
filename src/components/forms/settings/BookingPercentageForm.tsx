113e75e4b3d0b0672528bfdfb137cd6070bc1def
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-02-07 15:46:09 +0500

    adjusted: px

--- src/components/forms/settings/BookingPercentageForm.tsx
+++ src/components/forms/settings/BookingPercentageForm.tsx
@@ -4,7 +4,7 @@ import SaveBtn from './components/SaveBtn';
 export default function BookingPercentageForm({ ...rest }: FlexProps) {
   return (
     <Flex as={'form'} {...rest}>
-      <Flex flexDir={'column'} gap={4} w={'min(100%, 25rem)'} px={3}>
+      <Flex flexDir={'column'} gap={4} w={'min(100%, 25rem)'} px={2}>
         <FormControl>
           <FormLabel>Commission Percentage</FormLabel>
           <Input placeholder="Set Percentage" variant={'filled'} type="number" />
