d6abbbe0ad95853fcdbf8bc21aa3f559216274ea
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-02-07 20:14:19 +0500

    updated: dashboard

--- src/types/dashboard.ts
+++ src/types/dashboard.ts
@@ -1,19 +1,21 @@
 import { ReactElement } from "react";
 
 export interface DummyData {
-    items?: string;
-    for?: string;
-    icon?: ReactElement; 
-  };
+  items?: string;
+  for?: string;
+  icon?: ReactElement; 
+}
+
 export interface StatCardProps  {
-    data: DummyData;
-  };
+  data: DummyData;
+}
+
 export interface DummyTableData {
-    ORGANIZATIONNAME?: string;
-    VEHICLETYPE?: string;
-    STARTEDAT?: string; 
-    PICKUPLOCATION?: string; 
-    DESTINATIONLOCATION?: string; 
-    STATUSData?: string; 
-    STATUSVALUE?:'pending' | 'cancelled' | 'completed';
-  };
+  ORGANIZATIONNAME?: string;
+  VEHICLETYPE?: string;
+  STARTEDAT?: string; 
+  PICKUPLOCATION?: string; 
+  DESTINATIONLOCATION?: string; 
+  STATUSData?: string; 
+  STATUSVALUE?:'pending' | 'cancelled' | 'completed';
+}
